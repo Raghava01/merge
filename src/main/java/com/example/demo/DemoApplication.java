dccf4980afd8c67f2c776dad39aa0932bb5d1a5d
Raghava01 <45429841+Raghava01@users.noreply.github.com>
2023-04-12 18:48:39 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//demo
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
